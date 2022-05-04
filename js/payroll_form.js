a74807b7b00feda7ddfeebe98dcf0404766abec1
jayant <jayantparmar420@gmail.com>
2022-05-04 16:41:50 +0530

    Adding the ability to Reset the employee Registration form

--- js/payroll_form.js
+++ js/payroll_form.js
@@ -69,7 +69,7 @@ window.addEventListener('DOMContentLoaded', (event) =>{
         let employeePayrollList = JSON.parse(localStorage.getItem("EmployeePeyrollList"));
     
         if(employeePayrollList != undefined){
-            employeePayrollList.push(employeePayrollData);
+            employeePayrollList.push(EmployeePayrollData);
         }
         else{
             employeePayrollList = [employeePayrollData];
@@ -77,4 +77,33 @@ window.addEventListener('DOMContentLoaded', (event) =>{
     
         alert(employeePayrollList.toString());
         localStorage.setItem("EmployeePeyrollList",JSON.stringify(employeePayrollList));
+    }
+    
+    const resetForm = () => {
+        setValue('#name','');
+        unsetSelectedVlaues('[name=profile]');
+        unsetSelectedVlaues('[name=gender]');
+        unsetSelectedVlaues('[name=department]');
+        setValue('#salary','');
+        setValue('#notes','');
+        setValue('#day','1');
+        setValue('#month','1');
+        setValue('#year','2020');
+    }
+    
+    const setValue = (id,value) => {
+        const element = document.querySelector(id);
+        element.value = value;
+    }
+    
+    const setTextValue = (id,value) => {
+        const element = document.querySelector(id);
+        element.textContent = value;
+    }
+    
+    const unsetSelectedValues = (propertyValue) => {
+        let allItems = document.querySelectorAll(propertyValue);
+        allItems.forEach(item => {
+            item.checked = false;
+        });
     }
